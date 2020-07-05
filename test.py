a5df201536c72f9a3fa6a77efce2a8fbcaad961d
OptimisticAtom <optimisticatom@outlook.com>
2020-07-05 19:43:32 -0400

    stuff on time

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-print("Hi")
-print("Hi OptimisticAtom")
-#Hey can you put the code for when you want to time something
+import time
+# https://docs.python.org/3/library/time.html#time.time
+
+# current time in seconds since 1970
+print(time.time())
+
+# convert time in seconds into a readable string of characters
+print(time.localtime(time.time()))
+# Hey can you put the code for when you want to time something
