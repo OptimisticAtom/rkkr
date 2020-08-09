1c7646b409baa87a7610fa1ab36d334c2b94d47a
OptimisticAtom <optimisticatom@outlook.com>
2020-08-09 13:18:22 -0400

    e

--- test.py
+++ test.py
@@ -1,12 +1,30 @@
-import time
+# import time
+import os
 # https://docs.python.org/3/library/time.html#time.time
 
 # current time in seconds since 1970
-print(time.time())
+# print(time.time())
 
 # convert time in seconds into a readable string of characters
-print(time.ctime(time.time()))
+# print(time.ctime(time.time()))
 
 
 # the hour of the day
-print(time.localtime(time.time()).hour)
+# print(time.localtime(time.time()).hour)
+
+file = open('tehfile', 'a')
+if(not(os.path.exists('/home/kyle/Documents/Programming/Python/countFile'))):
+    countFileNew = open('count', 'w')
+    countFileNew.write('0')
+    countFileNew.close()
+
+
+countFile = open('count', 'w+')
+count = int(countFile.read())
+for i in range(count, count + 10):
+    file.write('meme: ' + str(i) + '  ')
+    newCount = i
+# hi
+countFile.write(str(newCount))
+countFile.close()
+file.close()
